e19e2e82321616e6a528a003946f42f693d12107
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-13 13:52:16 -0400

    Updated from Brython Server: 9/13/2017 1:52:14 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,15 @@
+#Jack Robey
+#9/13/17
+#gradeCalculator.py - calculates one's grade
 
+percentage = int(input('Enter your grade: '))
+if percentage>=90:
+    print('You earned an A')
+elif percentage>=80:
+    print('You eaned a B')
+elif percentage>=70:
+    print('You eaned a C')
+elif percentage>=60:
+    print('You eaned a D')
+else:
+    print('Your are failing')
