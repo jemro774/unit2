ce7bbf57d84e7e5866626dd63edc4fc2cdd7d80f
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-13 13:58:30 -0400

    Updated from Brython Server: 9/13/2017 1:58:29 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,14 @@
+#Jack Robey
+#9/13/17
+#movie.py - tells the user the most scandulous type of movie they can watch
+
+ageUser = int(input('Enter your age: '))
+if ageUser>=17:
+    print('You can watch R-rated movies')
+elif ageUser>=13:
+    print('You can watch PG-13 movies')
+elif ageUser>=5:
+    print('You can watch PG-rated movies')
+else:
+    print('You can watch G-rated movies')
 
