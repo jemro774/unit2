b27d6cc58e8c09f587b524254e7bd955bd4898fc
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-15 14:45:00 -0400

    Updated from Brython Server: 9/15/2017 2:44:59 PM

--- unitConverter.py
+++ unitConverter.py
@@ -11,11 +11,16 @@ num = int(input('Choose a unit conversion by entering in one of the displayed nu
 
 if num == 1:
     km = float(input('Enter the amount of kilometers: '))
-    print('There are', round(km*0.62137119, 2), 'miles in', km, 'kilometers')
+    print('There are', round(km*0.62137119223733, 2), 'miles in', km, 'kilometers')
 elif num == 2:
     kg = float(input('Enter the amount of kilograms: '))
-    print('There are', round(kg*2.2046226218, 2), 'pounds in', kg, 'kilometers')
+    print('There are', round(kg*2.2046226218488, 2), 'pounds in', kg, 'kilometers')
 elif num == 3:
     li = float(input('Enter the amount of liters: '))
     print('There are', round(li*0.26417205235815, 2), 'gallons in', li, 'liters')
+elif num == 4:
+    cel = float(input('Enter a temperature in celcius: '))
+    print(cel, 'degrees Celcius is equal to', round(cel*1.8+32, 2), 'degrees Fahrenheit')
+else:
+    print('Error, invalid number entered')
 
