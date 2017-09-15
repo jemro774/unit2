af50bc45dec112f38fdf5f5848bfcac4d8fb7bfe
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-15 14:55:14 -0400

    Updated from Brython Server: 9/15/2017 2:55:13 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -1,1 +1,17 @@
+#Jack Robey
+#9/15/17
+#fortuneTeller.py - displays fortune based on the color and number combination picked
+
+colr = input('Pick red or blue: ')
+num = int(input('Pick a number from 1 to 4: '))
+if colr == 'red' and num == 1:
+    print('You will inherit five million dollars')
+elif colr == 'red' and num == 2:
+    print('You will lose in chess')
+elif colr == 'red' and num == 3:
+    print('You will fall off a bridge')
+elif colr == 'red' and num == 4:
+    print('You will win the lottery')
+elif colr == 'blue' and num == 1:
+    print('You will go blind')
 
