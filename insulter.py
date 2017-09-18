08918c2ff0e745d5240b548d0b2b072297ca3c73
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-18 14:50:45 -0400

    Updated from Brython Server: 9/18/2017 2:50:44 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,20 @@
+#Jack Robey
+#9/18/17
+#insulter.py - asks the user to enter their name and then generates a random insult
+
+from random import randint
+nameUser = input('Enter your name: ')
+num = randint(1,5)
+if num == 1:
+    print('You suck at computer programming')
+elif num == 2:
+    print('You will never be the man your mother is')
+elif num == 3:
+    print('Stop whining')
+elif num == 4:
+    print('We all sprang from apes, but you did not spring far enough')
+elif num == 5:
+    print('I would ask how old you are, but I know you cannot count that high')
+else:
+    print('Error')
 
