f3fd044f69e1f274337927c3e2e6d35c87977bdc
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-25 14:36:42 -0400

    Updated from Brython Server: 9/25/2017 2:36:41 PM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,13 @@
+#Jack Robey
+#9/25/17
+#quiz2.py - prompts user to enter in 2 numbers and tells him/her properties of the 2 numbers
 
+num1 = int(input('Enter a number: ))
+num2 = int(input('Enter a second number: ))
+
+if num1>num2:
+    print('The first number is bigger')
+elif num2>num1:
+    print('The second number is bigger')
+else:
+    print('The numbers are the same')
